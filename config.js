478a85ec66d5b681f50d8682fc511e23e1cbaa85
maks2197 <maks2197@gmail.com>
2026-05-31 22:40:29 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycby96VZp9XVxVWko2Lw59pfd5xbzcqjozazew61NraI6-dSat8O6wUEnsU4wmPCKV6N3/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxbcHdiU89wJ4w4BKFQldAuKo1qXjSOgjSQPYe18IWyXf26hoZ_ICiTTH9V5ypsKizR/exec',
   ADMIN_PASS: 'tdtk2026_secure', // Должен совпадать с Apps Script
   IS_PREVIEW: false, // true = демо, false = реальная таблица
   CALC: { base: 70000, sub: 65000, monthly: 200 }
