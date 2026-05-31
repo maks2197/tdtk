22079a43d082803e219e42cb3987194fa3134a80
maks2197 <maks2197@gmail.com>
2026-05-31 12:39:50 +1100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  API_URL: 'ВСТАВЬТЕ_СЮДА_URL_ВАШЕГО_APPS_SCRIPT',
+  ADMIN_PASS: 'tdtk2026_secure', // Должен совпадать с Apps Script
+  IS_PREVIEW: true, // true = демо, false = реальная таблица
+  CALC: { base: 70000, sub: 35000, monthly: 200 }
+};
